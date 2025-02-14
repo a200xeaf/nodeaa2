073d8958ff6352c9d74d8c2d423748be94cad5f1
a200xeaf <a200xeaf@protonmail.com>
2025-02-14 14:08:52 -0800

    new baseline

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     target: 'esnext',
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,    // remove console.* calls
+        drop_debugger: true,   // remove debugger statements
+      },
+    }
   },
   plugins: [react()],
   resolve: {
