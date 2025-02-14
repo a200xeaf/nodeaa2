d0a74a28ca7ea80294ff1a65125e96012a67e434
a200xeaf <a200xeaf@protonmail.com>
2025-02-14 13:17:18 -0800

    terser compression

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    sourcemap: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        // Bundle node_modules separately for better caching
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    }
   },
   plugins: [react()],
   resolve: {
